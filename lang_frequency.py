b2c1f2bc2aa9400ba39cb59cc0a13f9baadad0b9
mfgnik <31517000+mfgnik@users.noreply.github.com>
2018-02-21 16:26:51 +0300

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -9,11 +9,11 @@ def load_data(filepath):
 
 
 def get_most_frequent_words(text):
-    c = collections.Counter()
+    counter = collections.Counter()
     for word in text:
-        c[word] += 1
-    return c.most_common(10)
+        counter[word] += 1
+    return counter.most_common(10)
 
 if __name__ == '__main__':
     file_path = input()
-    print(*(x[0] for x in get_most_frequent_words(load_data(file_path))))
+    print(*(word[0] for word in get_most_frequent_words(load_data(file_path))))
